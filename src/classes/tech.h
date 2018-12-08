270b79270d2cbfce505310725961b8201add08d9
Anthony Cagliano <bluiitalian09@aol.com>
2018-12-07 22:27:30 -0500

    Slight additions to the tech modules. Not entirely sure how to organize that.

--- src/classes/tech.h
+++ src/classes/tech.h
@@ -2,6 +2,11 @@
 #ifndef tech_h
 #define tech_h
 
+enum TechClasses {
+    TECHCLASS_SHIPMODULE,
+    TECHCLASS_PROJECTILE
+};
+
 enum TechTypes {
     TECHTYPE_SHIELD = 1,
     TECHTYPE_INTEG,
@@ -16,12 +21,24 @@ enum TechTypes {
     TECHTYPE_TORPEDO
 };
 
+enum ProjectileTypes {
+    PROJECTILE_SPATIAL = 1,
+    PROJECTILE_PHOTON,
+    PROJECTILE_QUANTUM,
+    PROJECTILE_ANTIMATTER,
+    PROJECTILE_CHRONITON,
+    PROJECTILE_TRANSPHASIC,
+    PROJECTILE_NEROMAIN,
+    PROJECTILE_NEROSPAWN
+};
 
 typedef struct {
+    int powerdrain;
     int protection;
 } shield_stats_t;
 
 typedef struct {
+    int powerdrain;
     int protection;
     int weight;
 } integrity_stats_t;
@@ -35,6 +52,7 @@ typedef struct {
 } engine_stats_t;
 
 typedef struct {
+    int powerdrain;
     int targetxz;       // angles for targetting
     int targetxy;       // angles for targetting
     int maxrange;
@@ -54,7 +72,20 @@ typedef union {
     shield_stats_t shield_stats;
     generator_stats_t generator_stats;
     engine_stats_t engine_stats;
-} stats_t;
+} module_stats_t;
 
+typedef union {
+    module_stats_t module_stats;
+    projectile_stats_t projectile_stats;
+} sub_stats_t;
+
+
+typedef struct {
+    int techid;     // this is just its place in array, but also its id
+    int techclass;   // ship module, projectile, etc
+    int techtype;   // used to determine if compatible
+    int powerdrain; // how much base power this module uses
+    sub_stats_t techinfo;    // import tech-specific info
+} stats_t;
 
 #endif
