d653c964f71baaa3a671b6ec5222a2131ec6b261
Anthony Cagliano <bluiitalian09@aol.com>
2019-03-18 18:40:18 -0400

    Revised background image, tab highlighting added.

--- src/main.c
+++ src/main.c
@@ -34,22 +34,39 @@
 #include "statscreens.h"
 #include "gfx/trekgui.h"
 /* Put your function prototypes here */
+void DrawFrame(unsigned char screen);
 
 static ship_t Ship;
 
 void main(void) {
     unsigned char screen = 0;
+    bool loopgame = true;
     unsigned int i;
     int_Disable();
     gfx_Begin();
     gfx_SetDefaultPalette(gfx_8bpp);
+    gfx_SetDrawBuffer();
     if(!trekgui_init()) return;
-    Screen_Background(screen);
-    while(!os_GetCSC());
+    do {
+        unsigned char key = os_GetCSC();
+        DrawFrame(screen);
+        if(key == sk_Clear) loopgame = false;
+        if(key == sk_Yequ) screen = 0;
+        if(key == sk_Window) screen = 1;
+        if(key == sk_Zoom) screen = 2;
+        if(key == sk_Trace) screen = 3;
+        if(key == sk_Graph) screen = 4;
+    } while(loopgame == true);
     gfx_End();
     int_Enable();
     pgrm_CleanUp();
+    return;
 }
 
+void DrawFrame(unsigned char screen){
+    Screen_Background(screen);
+    gfx_BlitBuffer();
+    return;
+}
 /* Put other functions here */
 /* Draw text on the homescreen at the given X/Y location */
