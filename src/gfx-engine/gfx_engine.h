1ed295ed72f9b8ac92f57573d2c7dcaa264900cf
Anthony Cagliano <ac@clrhome.org>
2019-05-01 19:37:24 -0400

    Start python server scripting. Add structure setup for rendering engine.

--- src/gfx-engine/gfx_engine.h
+++ src/gfx-engine/gfx_engine.h
@@ -3,32 +3,31 @@
 #define gfx_engine_h
 
 // ENGINE TYPEDEFS
-typedef struct {
-    long x;
-    long y;
-    long z;
-} rend_point_t;
-rend_point_t P_AddVectorToPoint(rend_point_t *point, rend_vector_t *vector);
-rend_point_t P_SubVectorFromPoint(rend_point_t *point1, rend_point_t *point2);
-rend_vector_t P_SubPointFromPoint(rend_point_t *point1, rend_point_t *point2);
-void P_SetPointToPoint(rend_point_t *point, long x, long y, long z);
+enum Objects {
+    STAR,
+    PLANET,
+    STARBASE,
+    ASTEROID,
+    COMET,
+    BLACKHOLE,
+    SHIP,
+    TORPEDO,
+    PHASER,
+    EJ_CORE,
+};
 
-typedef rend_point_t rend_vector_t;
-rend_vector_t V_AddVectorToVector(rend_vector_t *vector1, rend_vector_t *vector2);
-rend_vector_t V_SubVectorFromVector(rend_vector_t *vector1, rend_vector_t *vector2);
-rend_vector_t V_RotateXY(char angle);
-rend_vector_t V_RotateYZ(char angle);
-rend_vector_t V_RotateXZ(char angle);
-rend_vector_t V_Scale(int s0, int s1, int s2);
+enum Particles {
+    DMG_INT,    // Sparks that fly when internal damage occurs (renders to player)
+    DMG_EXT,    // Smoke trail that renders in space when ship damage (renders to other players)
+    EXPLODE     // Explosions (any kind)
+}
 
 typedef struct {
-    int minX, maxX;     // drawing window X
-    int minY, maxY;     // drawing window Y
-    char renderAngle;   // FOV
-    int renderdist;     // camera depth
-} rend_camera_t;
-void CAM_InitTo1stP(rend_camera_t *camera, rend_point_t *point);
-void CAM_InitTo3rdP(rend_camera_t *camera, rend_point_t *vector);
-void CAM_CullMap(rend_camera_t *camera, MapData_t *map);
+    unsigned char objectid;     // Cooresponds to enum Objects, and index of sprite
+    unsigned int radius;        // Radius of round object, or max size of irregular
+    bool irregular;             // True if object is irregular
+    int rand_seed;              // Seed used to generate object (if irregular) - server-supplied
+} mapentity_t;
+
 
 #endif
