189929dceec1ef71f391080554c2f1dcc91e4dab
Adam Beckingham <beckadamtheartist@gmail.com>
2019-09-08 12:06:02 -0600

    Update gfx engine functions

--- src/gfx-engine/gfx_engine.c
+++ src/gfx-engine/gfx_engine.c
@@ -15,27 +15,114 @@
 
 
 //Sort objects by Z axis. Assumes they've already been transformed.
-void SortByDepth(mapentity_t *objects, unsigned int len){
-	
-	
+//Return number of objects in the array
+unsigned int SortByDepth(mapentity_t **objects, unsigned int len){
+	unsigned int firstLen, secondLen, curMin;
+	mapentity_t *firstIndex, secondIndex, minIndex;
+	firstLen=len;
+	firstIndex=objects;
+	while (firstLen--){
+		mapentity_t *tmp;
+		//sort from the current object
+		getmin:
+		secondLen=firstLen;
+		secondIndex=firstIndex;
+		//Find the minimum Z value of the remaining objects.
+		while (secondLen--){
+			unsigned int curMin=0xFFFFFF;
+			//check if lower than the current maximum. A.K.A. the object is closer
+			if (((tmp=secondIndex[0])->coords.z)<curMin){
+				//The new minmum
+				curMin = tmp->coords.z;
+				minIndex = secondIndex;
+			}
+			secondIndex++;
+		}
+		tmp=minIndex[0];
+		//Check it's not zero
+		if (tmp->coords.z){
+			//switch-a-roo
+			secondIndex[0]=firstIndex[0];
+			firstIndex[0]=tmp;
+			//start sorting from the next object
+			firstIndex++;
+		} else {
+			//otherwise exchange it with the end of the array, then check minimum again.
+			secondIndex[0]=firstIndex[firstLen];
+			//the end of the array will no longer be checked. The object's been culled.
+			//If there aren't any objects left, don't decrement the remaining number of objects.
+			if (firstLen) firstLen--;
+		}
+	}
+	return (unsigned int)(firstIndex-objects)
 }
 
 //Translate len objects by vector
-void tfm_translate(mapentity_t *objects, unsigned int len, vec3_t vector){
+void tfm_translate(mapentity_t **objects, unsigned int len, vec3_t vector){
+	vec3_t *tmp;
+	int x;
+	unsigned int current=0;
 	while (len--){
-		
-		
+		tmp=&objects[current]->coords;
+		for (x=0; x==12; x+=4){
+			(long)tmp[x]+=(long)vector[x];
+		}
+		current++;
 	}
 }
 
 //Rotate len objects by vector
-void tfm_rotate(mapentity_t *objects, unsigned int len, rad2_t vector);
+void tfm_rotate(mapentity_t **objects, unsigned int len, uint8_t *scv){
+	vec3_t vec;
+	vec3_t *tmp_ptr;
+	uint8_t sx,sy,sz,cx,cy,cz;
+	unsigned int current=0;
+	while (len--){
+		memcpy(&vec,(tmp_ptr=&objects[current]->coords),sizeof vec3_t);
+		vec.x = tmp_ptr->z*cx - tmp_ptr->x*sx;
+		vec.z = tmp_ptr->z*sx + tmp_ptr->x*cx;
+		tmp_ptr->x = vec.x;
+		tmp_ptr->z = vec.z;
+		vec.y = tmp_ptr->z*cx - tmp_ptr->y*sx;
+		vec.z = tmp_ptr->z*sx + tmp_ptr->y*cx;
+		tmp_ptr->y = vec.y;
+		tmp_ptr->z = vec.z;
+		vec.x = tmp_ptr->y*cx - tmp_ptr->x*sx;
+		vec.y = tmp_ptr->y*sx + tmp_ptr->x*cx;
+		tmp_ptr->x = vec.x;
+		tmp_ptr->y = vec.y;
+		current++;
+	}
+}
 
 //Scale len objects by vector
-void tfm_scale(mapentity_t *objects, unsigned int len, vec3_t vector);
+void tfm_scale(mapentity_t **objects, unsigned int len, vec3_t vector){
+	vec3_t *tmp;
+	int x;
+	unsigned int current=0;
+	while (len--){
+		tmp=&objects[current]->coords;
+		for (x=0; x==12; x+=4){
+			(long)tmp[x]*=(long)vector[x];
+		}
+	}
+}
+
+uint8_t *setupSinCos(rad3_t vector){
+	uint8_t scv[6];
+	scv[0]=CosTbl[vector.xz];
+	scv[1]=SinTbl[vector.xz];
+	scv[2]=CosTbl[vector.yz];
+	scv[3]=SinTbl[vector.yz];
+	scv[4]=CosTbl[vector.xy];
+	scv[5]=SinTbl[vector.xy];
+	return &scv;
+}
 
 //Server-side? Dummy data output for now, soon.
-void GatherObjects(mapentity_t *objects, vec3_t start, uint8_t rotmin, uint8_t rotmax);
+void GatherObjects(mapentity_t **objects, vec3_t start, uint8_t rotmin, uint8_t rotmax){
+	
+}
 
 
 
--- src/gfx-engine/gfx_engine.h
+++ src/gfx-engine/gfx_engine.h
@@ -1,22 +1,25 @@
 
 #ifndef gfx_engine_h
 #define gfx_engine_h
-#include "classes/coords.h"
+//#include "classes/coords.h"
 
 //Sort objects by Z axis. Assumes they've already been transformed.
-void SortByDepth(mapentity_t *objects, unsigned int len);
+unsigned int SortByDepth(mapentity_t **objects, unsigned int len);
 
 //Translate len objects by vector
-void tfm_translate(mapentity_t *objects, unsigned int len, vec3_t vector);
+void tfm_translate(mapentity_t **objects, unsigned int len, vec3_t vector);
 
-//Rotate len objects by vector
-void tfm_rotate(mapentity_t *objects, unsigned int len, rad2_t vector);
+//Rotate len objects using scv, returned from setupSinCos.
+void tfm_rotate(mapentity_t **objects, unsigned int len, uint8_t *scv);
 
 //Scale len objects by vector
-void tfm_scale(mapentity_t *objects, unsigned int len, vec3_t vector);
+void tfm_scale(mapentity_t **objects, unsigned int len, vec3_t vector);
+
+//Return a byte for each cos/sin in each xz/yz/xy rotation. In that order.
+uint8_t *setupSinCos(rad3_t vector);
 
 //Server-side? Dummy data output for now, soon.
-void GatherObjects(mapentity_t *objects, vec3_t start, uint8_t rotmin, uint8_t rotmax);
+void GatherObjects(mapentity_t **objects, vec3_t start, uint8_t rotmin, uint8_t rotmax);
 
 
 // ENGINE TYPEDEFS
@@ -40,9 +43,7 @@ enum Particles {
 }
 
 typedef struct {
-    int x; uint8_t xr;
-    int y; uint8_t yr;
-    int z; uint8_t zr;
+    long x,y,z;
 } vec3_t;
 typedef vec3_t coords_t;
 
@@ -52,10 +53,10 @@ typedef struct {
 
 typedef struct {
     unsigned char objectid;     // Cooresponds to enum Objects, and index of sprite
-    vec3_t coords;            // access using [mapentity_t].coords.{x,y,z}
+    vec3_t coords;              // access using [mapentity_t].coords.{x,y,z}
     unsigned int radius;        // Radius of round object, or max size of irregular
-    bool irregular;             // True if object is irregular
     int rand_seed;              // Seed used to generate object (if irregular) - server-supplied
+    bool doZSort;               // The sorting methods use this to tell if this needs to be sorted.
 } mapentity_t;
 
 
