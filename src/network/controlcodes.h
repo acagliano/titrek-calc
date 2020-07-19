93421d9ac3dcca1b93af4760ab21d24cedf88886
Anthony Cagliano <ac@clrhome.org>
2020-07-19 18:52:06 -0400

    Add some notations to controlcodes.h, add a few control codes.

--- src/network/controlcodes.h
+++ src/network/controlcodes.h
@@ -2,17 +2,22 @@
 #define controlcodes_h
 
 enum ControlCodes {
-    REGISTER,
-    LOGIN,
-    DISCONNECT,
-    PGRMUPDATE,
-    GFXUPDATE,
-    SHIPDATAREQ,
-    MODULEINFO,
-    MOVESHIP,
-    REQCHUNK,
-    REQENTITY,
-    REQSENSDATA, // May add REQPOSITION if needed
+    // network handling
+    REGISTER,       // attempt to create new account
+    LOGIN,          // attempt to join existing account
+    DISCONNECT,     // attempt to disconnect from the server
+    // autoupdater
+    PGRMUPDATE,     // data segment for client self-update
+    GFXUPDATE,      // data segment for graphics update
+    // load game data
+    LOADGAME,       // receive necessary data to render GUI (ship_t)
+    // no need for a SAVEGAME because everything is server-side anyway
+    // in-game stuff
+    MODULEINFO,     // request all module information (instead of abbreviated)
+    MODULEUPDATE,   // used by server to send info to client. Also used by client at times.
+    REQUESTCHUNK,       // request chunk framedata
+    REQUESTENTITY,      // request entity framedata
+    REQUESTSENSOR,  // Ask server for (1) position, (2) rotation matrix, (3) sensor data
     PING = 0xfc,
     MESSAGE = 0xfd,
     DEBUG = 0xfe,
@@ -28,6 +33,21 @@ enum ResponseCodes {
     BANNED,
     VERSION_MISMATCH
 };
+
+/*
+    Module Update Notes
+    MODULEUPDATE
+*/
+enum ModuleUpdateCodes {
+// for inbound (server->client)
+    UPD_STATUS,     // update online/offline state
+    UPD_HEALTH,     // update saved health %
+    UPD_POWER    // update saved power %
+    UPD_REASSIGN // change entire module (usually replacing with new tech)
+}
+// client may also send a MODULEUPDATE request when "purchasing" an upgrade.
+// We may wind up storing upgrades that enhance existing modules (instead of be their own tech) in their own database, such that they may be requested by sending a MODULEUPDATE with the id.
+// Server-side, these upgrades will likely be stored as Name, Attribute to upgrade, amount of upgrade, cost.
 
 
 #endif
