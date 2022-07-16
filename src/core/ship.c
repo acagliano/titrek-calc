16687ddaef6e2471f1587e84993c265eda748f29
Anthony Cagliano <acagliano97@gmail.com>
2022-07-16 14:03:40 -0400

    begin to rework ship systems

---
+++ src/core/ship.c
@@ -0,0 +1,6 @@
+
+#include "ship.h"
+
+ship_system_data_t ship_systems[SHIP_MAX_MODULES] = {
+    
+}
---
+++ src/core/ship.h
@@ -0,0 +1,29 @@
+
+#ifndef ship_h
+#define ship_h
+
+#define SHIP_MAX_MODULES 16
+#define MODULE_LOCKED_PTR_SET ((void*)0xffffff)
+
+enum _system_types {
+    SYS_EMPTY,
+    SYS_CORE,
+    SYS_WEAPON,
+    SYS_DEFENSE,
+    SYS_MISC,
+    SYS_SLOT_UNAVAILABLE = 0xff
+}
+
+
+typedef struct _ship_system_data {
+    uint8_t system_type;
+    uint8_t online_status;  // online/offline
+    uint8_t system_name[11];
+    uint8_t system_health_percent;
+    uint8_t system_power_percent;
+} ship_system_data_t;
+extern ship_system_data_t ship_systems[SHIP_MAX_MODULES];
+
+
+
+#endif
