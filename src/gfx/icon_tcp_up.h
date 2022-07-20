7dfd80bbfa168e04730bdef188931702915dcdee
Anthony Cagliano <acagliano97@gmail.com>
2022-07-19 22:48:48 -0400

    add in new sprite files

---
+++ src/gfx/icon_tcp_up.h
@@ -0,0 +1,18 @@
+#ifndef icon_tcp_up_include_file
+#define icon_tcp_up_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define icon_tcp_up_width 16
+#define icon_tcp_up_height 16
+#define icon_tcp_up_size 167
+#define icon_tcp_up_compressed_size 46
+extern unsigned char icon_tcp_up_compressed[46];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
