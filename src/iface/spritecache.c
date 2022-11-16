d09917a85139581f5176303a4299d8add8f8eb23
Anthony Cagliano <acagliano97@gmail.com>
2022-11-16 14:16:01 -0500

    some handling changes to gfx cache

--- src/iface/spritecache.c
+++ src/iface/spritecache.c
@@ -1,9 +1,14 @@
 #include <stdbool.h>
 #include <graphx.h>
 #include <fileioc.h>
+#include "spritecache.h"
 
 uint8_t* gfx_cache_file;
 const char gfx_fname = "_trekgfx";
+
+size_t gfx_cache_ct = 0;
+gfx_rletsprite_t *gfx_cache_ptrs[GFX_CACHE_MAX] = {NULL};
+
 
 bool sprite_open_cache(void){
 	return (gfx_cache_file = ti_Open(gfx_fname, "w+"));
@@ -11,7 +16,11 @@ bool sprite_open_cache(void){
 
 gfx_rletsprite_t *sprite_append_to_cache(uint8_t *data, size_t len){
 	gfx_rletsprite_t *sprite_ptr = ti_GetDataPtr(gfx_cache_file);
-	if(ti_Write(data, len, 1, gfx_cache_file)) return sprite_ptr;
+	if(gfx_cache_ct==GFX_CACHE_MAX) return NULL;
+	if(ti_Write(data, len, 1, gfx_cache_file)) {
+		gfx_cache_ct++;
+		return sprite_ptr;
+	}
 	else return NULL;
 }
 
--- src/iface/spritecache.h
+++ src/iface/spritecache.h
@@ -6,8 +6,12 @@
 #include <graphx.h>
 #include <fileioc.h>
 
+#define GFX_CACHE_MAX	128
+extern size_t gfx_cache_ct;
+extern gfx_rletsprite_t *gfx_cache_ptrs[GFX_CACHE_MAX];
 
-
+bool sprite_open_cache(void);
+bool sprite_close_cache(void);
 gfx_rletsprite_t *sprite_append_to_cache(uint8_t *data, size_t len);
 
 
